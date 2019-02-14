cba5d25566118800794d68a41ace12100ea36dfa
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-02-14 21:41:32 +0100

    skip pull request during dry run

--- run_mode.go
+++ run_mode.go
@@ -22,6 +22,7 @@ func (run dryRun) process(i *github.Issue) {
 	if i.IsPullRequest() {
 		run.stats.PullRequest++
 		fmt.Println(fmt.Sprintf("skip %s: is pull request", i.GetHTMLURL()))
+		return
 	}
 
 	if !isStale(i) {
